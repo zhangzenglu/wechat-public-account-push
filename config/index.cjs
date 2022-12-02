9b1e3f1a5fde2fd26b6451512dd38957d54c53ec
zhangzenglu <119598380+zhangzenglu@users.noreply.github.com>
2022-12-02 11:15:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -45,7 +45,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '0002',
+  CALLBACK_TEMPLATE_ID: 'YWUouMkfWQF1rDw8BPo7lFBxtPXGDZ3c2gjPODoZYZY 	',
 
   CALLBACK_USERS: [
     {
