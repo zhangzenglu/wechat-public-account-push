0369c529079408b0edb0198a2d8b47962e72c3f8
zhangzenglu <119598380+zhangzenglu@users.noreply.github.com>
2022-12-02 11:47:55 +0800

    Create template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -2,24 +2,39 @@
 const TEMPLATE_CONFIG = [
   {
     id: '0TGvdZrr0KFVWhxDaxQ_UvTJPr7Yw7TwGB7rAFV1Nho',
-    title: '宝宝, 早上好呀',
+    title: '亲爱的, 早上好',
     desc: `
-      {{date.DATA}}
+      **{{date.DATA}}**
+      
       下个休息日：{{holidaytts.DATA}}
+      
       ---
+      
       城市：{{city.DATA}}
+      
       天气：{{weather.DATA}}
+      
       气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
+      
       风向: {{wind_direction.DATA}}
+      
       风级: {{wind_scale.DATA}}
+      
       {{comprehensive_horoscope.DATA}}
+      
       ---
+      
       今天是我们相识的第{{love_day.DATA}}天
+      
       {{birthday_message.DATA}}
+      
       ---
+      
       {{moment_copyrighting.DATA}}
+      
       {{poetry_title.DATA}} {{poetry_content.DATA}}
     `
+  },
   {
     id: 'YWUouMkfWQF1rDw8BPo7lFBxtPXGDZ3c2gjPODoZYZY 	',
     title: '推送完成提醒',
