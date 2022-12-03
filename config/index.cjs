c48354ab7be55ca1706707529cf569331a36fe5d
zhangzenglu <119598380+zhangzenglu@users.noreply.github.com>
2022-12-03 20:57:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -59,7 +59,7 @@ USERS: [
       // 想要发送的人的名字
       name: '罗怡姐',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'onBIn6msn5XCyOnRG5wa8HC93MlY',
+      id: 'onBIn6vJGNk564uk36J7rhSQv1JI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       province: '海南',
   city: '海口',
@@ -84,7 +84,7 @@ customizedDateList: [
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'YWUouMkfWQF1rDw8BPo7lFBxtPXGDZ3c2gjPODoZYZ',
+  CALLBACK_TEMPLATE_ID: 'YWUouMkfWQF1rDw8BPo7lFBxtPXGDZ3c2gjPODoZYZY',
 
   CALLBACK_USERS: [
     {
