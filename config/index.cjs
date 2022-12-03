70ccebf6647f66bbb85de9af96e147aa3653f99b
zhangzenglu <119598380+zhangzenglu@users.noreply.github.com>
2022-12-04 06:45:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -59,7 +59,7 @@ USERS: [
       // 想要发送的人的名字
       name: '罗怡姐',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'onBIn6vJGNk564uk36J7rhSQv1JI',
+      id: 'onBIn6vJGNk564uk36J7rhSQv1J',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       province: '海南',
   city: '海口',
