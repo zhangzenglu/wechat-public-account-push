cb59f03f634a28750b0cb0433a303a68cbfc9409
zhangzenglu <119598380+zhangzenglu@users.noreply.github.com>
2022-12-04 19:40:43 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -20,11 +20,11 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '娜娜公主',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'onBIn6pLNVryXQEhII3EUZHa-3s',
+      id: 'onBIn6pLNVryXQEhII3EUZHa-3sM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       province: '江苏',
   city: '南京',
-      useTemplateId: 'K4lVST1Rxo0EevlcRQ3psJ9hrcFo_ETZS6Ts1AISgfg',
+      useTemplateId: 'eOm2oZW8uJVueOAzFkNayEaAdAk7if91BMpn9rSObbY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-21',
       festivals: [
@@ -44,7 +44,7 @@ customizedDateList: [
   
 {
       name: '罗怡公主',
-      id: 'onBIn6vJGNk564uk36J7rhSQv1J',
+      id: 'onBIn6vJGNk564uk36J7rhSQv1JI',
       useTemplateId: 'SXvoV3DAKWpf5fBCeorRS2bh2n2r5VrAijmKNm_lAxM',
       province: '海南',
       city: '海口',
@@ -64,7 +64,7 @@ customizedDateList: [
     {
       name: '海狗国王',
       id: 'onBIn6msn5XCyOnRG5wa8HC93MlY',
-      useTemplateId: 'SXvoV3DAKWpf5fBCeorRS2bh2n2r5VrAijmKNm_lAxM',
+      useTemplateId: 'U-pdfaHYb1iud1AxklDPDeQKiwjSEHVournfygxeZdY',
       province: '黑龙江',
       city: '牡丹江',
       horoscopeDate: '11-16',
@@ -82,8 +82,8 @@ customizedDateList: [
     },
     {
       name: '洋子王子',
-      id: 'onBIn6q0mO4ydWRu6xiKWZO194N',
-      useTemplateId: '1fc10-haOJExnlv-1e90bgpQ3pmqR3S0N_qIf-z_kIU',
+      id: 'onBIn6q0mO4ydWRu6xiKWZO194N4',
+      useTemplateId: 'DkprwIKZW6DIzUEBpNEuKbQLa9VHpJcoq1R6AC7PY0k',
       province: '黑龙江',
       city: '佳木斯',
       horoscopeDate: '02-07',
